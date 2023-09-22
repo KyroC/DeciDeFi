db0457579345eeba0b4eed79bc6909dbba1afbcf
KyroC <Kyroeth@gmail.com>
2023-09-22 19:40:32 +0800

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dex from './components/dex'
 import Home from './components/home'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 
 function App() {
   return (
     <div className="App">
-      <div className="Navbar">Nav</div>
+      <Navbar >
+      <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+      </Navbar>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
