3cc4f086cd7f3446f583dec0017249bbb4647712
KyroC <Kyroeth@gmail.com>
2023-09-22 14:30:20 +0800

    Created main.js components

---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Main() {
+    return(
+        <>
+            <Container>
+                <Row>
+                    <Col>New Pools</Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default Main;
