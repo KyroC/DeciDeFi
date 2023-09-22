227b37991c2aacb2b9e5d89877edc08fa23cde50
KyroC <Kyroeth@gmail.com>
2023-09-22 14:29:58 +0800

    Added routes and relevant bootstrap imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from './components/main'
 
 
 function App() {
   return (
     <div className="App">
-      Tracker
+      <div className="Navbar">Nav</div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
