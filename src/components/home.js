db419b222cd30296b0112c09b8a9bbf99e448b31
KyroC <Kyroeth@gmail.com>
2023-09-24 19:57:01 +0800

    Added sort by price function

--- src/components/home.js
+++ src/components/home.js
@@ -35,8 +35,12 @@ function Home() {
                 )  
     },[])
 
-    const handleSortPrice = () => {
-        
+    const handleSortPrice = (e) => {
+        e.preventDefault()
+        const sortingArray = sortedCoins.sort(
+            (p1,p2) => (p1.current_price < p2.current_price) ? 1 : (p1.current_price > p2.current_price) ? - 1 : 0 );
+            setSortedCoins([...sortingArray])
+            console.log(sortingArray)
     }
     return(
         <>
@@ -44,7 +48,7 @@ function Home() {
                 <Row className="justify-content-center">
                     <Col xl="4" lg="6" md="8">
                         <Card style={{height:'18rem'}}>
-                        <Card.Header><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-fire" viewBox="0 0 16 16">
+                        <Card.Header><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-fire" viewBox="0 0 16 16">
                             <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16Zm0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15Z"/>
                             </svg>Trending Coins  
                         </Card.Header>
@@ -78,7 +82,7 @@ function Home() {
                     <Col md="8" lg="6" xl="4" > 
                         <Card style={{height:'18rem'}}>
                         <Card.Header>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-fire" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-fire" viewBox="0 0 16 16">
                             <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16Zm0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15Z"/>
                             </svg>Trending NFTs
                          </Card.Header>
@@ -120,7 +124,7 @@ function Home() {
                 className="ms-auto border rounded"
                 variant="Secondary">
                     <Dropdown.Item>Market Cap</Dropdown.Item>
-                    <Dropdown.Item>Price</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => handleSortPrice(e)}>Price</Dropdown.Item>
                     <Dropdown.Item>24H Change</Dropdown.Item>
                 </DropdownButton>
             </Stack>
@@ -137,7 +141,7 @@ function Home() {
                     </tr>
                 </thead>
                 <tbody>
-                    {coins.map(coin => {
+                    {sortedCoins.map(coin => {
                         return(
                             <tr>
                                 <td>{coin.symbol.toUpperCase()}</td>
